1a870094f11e1ce401b9f9847ca8712b26273653
flege <abd.alfaruq@gmail.com>
2019-03-18 12:34:46 +0700

    Updates

---
+++ precache-manifest.4e7c341ebb5e10b6239fd33c36468aaf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/react/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "4e6a4b677d1c1e6ba359",
+    "url": "/react/static/js/runtime~main.bb50a20d.js"
+  },
+  {
+    "revision": "22a2d3f8348692c1413f",
+    "url": "/react/static/js/main.8bb0ba09.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/react/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "22a2d3f8348692c1413f",
+    "url": "/react/static/css/main.2b168a39.chunk.css"
+  },
+  {
+    "revision": "6345e541738dc76bbbef75a2104fc8f9",
+    "url": "/react/index.html"
+  }
+];
